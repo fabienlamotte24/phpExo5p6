0baf7e5c9ddcda0a54f24210f3b1f7a3fd95ad1f
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-30 15:51:47 +0200

    exo5p6 php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 5</title>
+  </head>
+  <body>
+    <p>
+<?php
+  echo $_GET['week'];
+?>
+    </p>
+  </body>
+</html>
